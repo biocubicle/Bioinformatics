0846cfc670f0f75d911135e4331147376ab7540a
biocubicle <108726368+biocubicle@users.noreply.github.com>
2022-07-05 10:36:05 +0100

    Pattern Count

--- Pattern Count algo.py
+++ Pattern Count algo.py
@@ -6,7 +6,7 @@ def PatternCount(sequence,pattern): #def allows us to define a function (in this
     for i in range(len(sequence)-len(pattern)+1): # according to this statement, for a position i in the sequence from starting position, which is zero in python to (e.g., if an Oric_seq has ten bases and pattern has 3), the endpoint of 8. This is done as from the 8th to the 10th position is the last reading frame of 3-mer; after that, shifting again to the 9th position will not yield a 3-mer sequence, as a result of which we might get an error.
         if sequence [i:i +len(pattern)] == pattern: #following the defined range, this if statement allows us to check if that pattern is present in the sequence
             count = count +1 # every time we find a match, just add to this variable that we previously defined
-    return count #So that the updated value as a result of function execution is available for use
+    return count #So that the updated value as a result of function execution is available for use.
 
 #Step II: Take inputs from the user for the sequence
 #Take an input sequence from the user
