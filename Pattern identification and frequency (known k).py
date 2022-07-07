edd84ebf6580b8ebd30e1b3ba9edbc5cc1e58de2
biocubicle <108726368+biocubicle@users.noreply.github.com>
2022-07-07 14:56:03 +0100

    Update Pattern identification and frequency (known k).py

--- Pattern identification and frequency (known k).py
+++ Pattern identification and frequency (known k).py
@@ -1,14 +1,9 @@
 ####Finding the frequency of all possible patterns of a known length k in a DNA sequence #####
-#Step II: Define a function that can identify frequent words of a particular length k from a given sequence
-def FrequentWords(Text, k):
-    words = [] # This is an empty list in which we will end up storing these frequent words of a particular known length k
-    freq = FrequencyMap(Text, k)  #This variable will call a function that has been defined below and use it on the given input (seq and len of k)
-    m = max(freq.values()) #We want to get the k-mer with a maximum frequency and hence are using max(). .values() allows us to access the values of the frequencies only instead of the recognised kmers.
-    for key in freq:
-        if freq[key] == m: #if a key has a maximum frequency,
-            words.append(key) #add that key to the list called words
-        # add each key to words whose corresponding frequency value is equal to m
-    return words
+
+#Input - DNA sequence and the length of the pattern that we are looking for 
+#Output - A list of all the possible patterns and their frequency; from which we then print the patterns that appear maximum number of times. 
+
+
 #Step I: Define a function and assocaited conditions/ loops to obtain a dictionary that resembles a frequency table. This is where we will see all the possible sequence and their assocaited frequencies.
 def FrequencyMap(Text, k):
     freq = {} # This is an empty dictionary which in this case will act like a frequency table in which a particular kmer/ pattern is stored in association with its frequency
@@ -23,6 +18,18 @@ def FrequencyMap(Text, k):
         else:
             freq[Pattern] = 1 #else, since it is not repeating again, simply add one. This is done because for the pattern to exist in the dictionary in the first place, it needs to occur atleast once.
     return freq
+
+#Step II: Define a function that can identify frequent words of a particular length k from a given sequence
+def FrequentWords(Text, k):
+    words = [] # This is an empty list in which we will end up storing these frequent words of a particular known length k
+    freq = FrequencyMap(Text, k)  #This variable will call a function that has been defined below and use it on the given input (seq and len of k)
+    m = max(freq.values()) #We want to get the k-mer with a maximum frequency and hence are using max(). .values() allows us to access the values of the frequencies only instead of the recognised kmers.
+    for key in freq:
+        if freq[key] == m: #if a key has a maximum frequency,
+            words.append(key) #add that key to the list called words
+        # add each key to words whose corresponding frequency value is equal to m
+    return words
+
 #Step III: Get the input of a sequence and length of k from the user.
 dna_seq =str((input('Plese enter the sequence for which you need to identify the kmer: ')))
 k_val = int((input('Kindly submit the length of the kmer that you are searching for: ')))
