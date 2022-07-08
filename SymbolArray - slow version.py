c7a2db802297eccef25ab94a982644b756b7debe
biocubicle <108726368+biocubicle@users.noreply.github.com>
2022-07-08 14:31:58 +0100

    Update SymbolArray - slow version.py

--- SymbolArray - slow version.py
+++ SymbolArray - slow version.py
@@ -12,18 +12,18 @@ def SymbolCount(Genome,symbol):
     return count
 #This function will allows us to get the frequency of the base in the genome. But now we need to add another function that will use the genome sequence and the SymbolCount function in the right manner.
 
-#Step II: Define a function to store the frequency and positions for that frequency in an array.
-def SymbolArray(Genome, symbol):
-    array = {}
+#Step II: Define a function to store the frequency and positions for that frequency in an ind.
+def Symbol(Genome, symbol):
+    ind = {}
     n = len(Genome)
     ExtendedGenome = Genome + Genome[0:n//2] #This line will ensure that the ends of the genome sequence are added again in the sequence to cover all the possible combinations for the window.
     for i in range(n):
-        array[i] = SymbolCount(ExtendedGenome[i:i+(n//2)],symbol) #ith element is equal to the frequency of that symbol in that window
-    return array
+        ind[i] = SymbolCount(ExtendedGenome[i:i+(n//2)],symbol) #ith element is equal to the frequency of that symbol in that window
+    return ind
 
 #Step III: Take an input from the user.
 gen = str(input('Please enter the genome sequence: '))
 sym = str(input('Please enter the base that you are trying to search: '))
 
 #Step IV: Call the function and obtain the results.
-print(SymbolArray(gen,sym))
+print(Symbol(gen,sym))
